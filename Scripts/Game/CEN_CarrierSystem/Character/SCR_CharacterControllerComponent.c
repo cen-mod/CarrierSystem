fc8ad571d75fa167840f8ddc942e881c648681b8
Kex <kexanone@gmail.com>
2023-11-22 01:08:40 +0100

    Release carried when they wake up

--- Scripts/Game/CEN_CarrierSystem/Character/SCR_CharacterControllerComponent.c
+++ Scripts/Game/CEN_CarrierSystem/Character/SCR_CharacterControllerComponent.c
@@ -2,15 +2,12 @@
 modded class SCR_CharacterControllerComponent : CharacterControllerComponent
 {
 	//------------------------------------------------------------------------------------------------
-	//! Release carried player if the carrier falls unconscious
+	//! Release carried player if they wake up or if the carrier falls unconscious
 	override protected void OnConsciousnessChanged(bool conscious)
 	{
 		super.OnConsciousnessChanged(conscious);
-		
 		ChimeraCharacter char = GetCharacter();
-		if (!char || !CEN_CarrierSystem_Helper.IsCarrier(char))
-			return;
-		
 		CEN_CarrierSystem_Helper.ReleaseFromCarrier(char);
+		CEN_CarrierSystem_Helper.ReleaseCarried(char);
 	}
 }
